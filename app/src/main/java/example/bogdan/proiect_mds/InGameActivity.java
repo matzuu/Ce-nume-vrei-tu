4ae79d7aefeee42d1131de6f6b9f45ce46267295
projectandu <stanescuandu24@gmail.com>
2017-05-26 23:03:21 +0300

    Am facut baza citirii, trebuie acum sa citesc proprietatile elementelor si sa desez pe ecran

--- app/src/main/java/example/bogdan/proiect_mds/InGameActivity.java
+++ app/src/main/java/example/bogdan/proiect_mds/InGameActivity.java
@@ -11,11 +11,19 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.IntBuffer;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class InGameActivity extends AppCompatActivity {
 
+    int n = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,7 +31,7 @@ public class InGameActivity extends AppCompatActivity {
         setContentView(new SampleView(this));
     }
 
-    private static class SampleView extends View implements Runnable {
+    private class SampleView extends View implements Runnable {
 
         Canvas canvas;
         Thread t = new Thread();
@@ -32,7 +40,29 @@ public class InGameActivity extends AppCompatActivity {
         public SampleView(Context context) {
             super(context);
             setFocusable(true);
+            int lvl = -1;
+            Bundle b = getIntent().getExtras();
+            if(b != null)
+                lvl = b.getInt("key");
+            String text;
+            try
+            {
 
+                Scanner sc = new Scanner(getAssets().open("Level" + lvl + ".txt"));
+                n = sc.nextInt();
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+
+            }
+
+        }
+
+        private void Toast(String content) {
+            Toast myToast = Toast.makeText(getApplicationContext(),content, Toast.LENGTH_LONG);
+            myToast.show();
         }
 
         @Override
@@ -43,6 +73,8 @@ public class InGameActivity extends AppCompatActivity {
             p.setColor(Color.BLACK);
             p.setStrokeWidth(4.5f);
             invalidate();
+            String textceva = "!!" + n;
+            Toast(textceva);
         }
         public void start()
         {
